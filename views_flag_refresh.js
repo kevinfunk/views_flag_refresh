0e4b2262670bc8815a831ad78adf6783066ff196
Chris Pliakas <cpliakas@266779.no-reply.drupal.org>
2010-08-31 02:19:34 +0000

    Added images, implemented widget functionality, removed theme.inc file since no theme functions are defined by this module.

--- views_flag_refresh.js
+++ views_flag_refresh.js
@@ -44,13 +44,15 @@ viewsFlagRefresh.ajaxPath = function() {
  * @param viewDisplayId
  *   The display ID of the view we are running the check against.
  * @return
- *   A boolean flagging whether the view is configured to be refreshed.
+ *   A string containing the theme hook used to theme the view as it is being
+ *   refreshed, a boolean false if the view should not be refreshed at all.
  */
 viewsFlagRefresh.prototype.refresh = function(viewName, viewDisplayId) {
-  for (var flagName in Drupal.settings.viewsFlagRefresh) {
+  var settings = Drupal.settings.viewsFlagRefresh.flags;
+  for (var flagName in settings) {
     functionName = viewName + '-' + viewDisplayId;
-    if (flagName == this.flagName && functionName in Drupal.settings.viewsFlagRefresh[flagName]) {
-      return true;
+    if (flagName == this.flagName && functionName in settings[flagName]) {
+      return settings[flagName][functionName];
     }
   }
   return false;
@@ -64,10 +66,11 @@ viewsFlagRefresh.prototype.refresh = function(viewName, viewDisplayId) {
  */
 viewsFlagRefresh.prototype.ajax = function(settings) {
   // Bails if the view shouldn't be refreshed when this flag is selected.
-  if (!this.refresh(settings.view_name, settings.view_display_id)) {
+  var themeHook = this.refresh(settings.view_name, settings.view_display_id);
+  if (!themeHook) {
     return;
   }
-      
+
   // Calculates the selector for the view.
   var view = '.view-dom-id-' + settings.view_dom_id;
   if (!$(view).size()) {
@@ -77,11 +80,22 @@ viewsFlagRefresh.prototype.ajax = function(settings) {
   // Locates the view, AJAX refreshes the content.
   $(view).filter(function() { return !$(this).parents('.view').size(); }).each(function() {
     var target = this;
+    
+    // Invokes the theme hook that calls the refresh widget, captures any
+    // elements added by the widget for cleanup.
+    var themeElement = viewsFlagRefresh.themeHookInvoke(themeHook, target);
+    
+    // Refreshes view via AJAX.
     $.ajax({
       url: viewsFlagRefresh.ajaxPath(),
       type: 'GET',
       data: settings,
       success: function(response) {
+        // Cleans up any elements left over from theming.
+        if (themeElement) {
+          $(themeElement).remove();
+        }
+        // Adds content retrieved from call.
         if (response.__callbacks) {
           $.each(response.__callbacks, function(i, callback) {
            // Temporary remove the ajax-form class to avoid behavior to be
@@ -92,8 +106,67 @@ viewsFlagRefresh.prototype.ajax = function(settings) {
           });
         }
       },
-      error: function() { alert(Drupal.t("An error occurred at @path.", {'@path': viewsFlagRefresh.ajaxPath()})); },
+      error: function() {
+        // Cleans up any elements left over from theming.
+        if (themeElement) {
+          $(themeElement).remove();
+        }
+        alert(Drupal.t("An error occurred at @path.", {'@path': viewsFlagRefresh.ajaxPath()}));
+      },
       dataType: 'json'
     });
+    
   });
+}
+
+/**
+ * Class method that invokes the theme hook.
+ * 
+ * @param themeHook
+ *   The theme hook being invoked.
+ * @param target
+ *   A jQuery object containing the view being refreshed. 
+ */
+viewsFlagRefresh.themeHookInvoke = function(themeHook, target) {
+  var theme = new viewsFlagRefresh.theme();
+  if (themeHook in theme) {
+    return theme[themeHook](target);
+  }
+  return false;
+}
+
+/**
+ * Contructor for our pseudo theme system.
+ */
+viewsFlagRefresh.theme = function() {
+  // Nothing to be done here.
+}
+
+/**
+ * Adds a 
+ * 
+ * @param target
+ *   A jQuery object containing the view being refreshed.
+ */
+viewsFlagRefresh.theme.prototype.throbber = function(target) {
+  // Hide the content of the view.
+  $(target).css('visibility', 'hidden');
+  
+  // Captures parent, as the view is usually in something such as a block.
+  var container = $(target).parent();
+  
+  // Adds our throbber to the middle of the view.
+  // NOTE: The throbber image is 32px wide.
+  var pos = $(container).position();
+  var throbber = $('<img src="' + Drupal.settings.viewsFlagRefresh.imagePath + '/throbber.gif" />')
+    .css('position', 'absolute')
+    .css('margin', '0')
+    .css('padding', '0')
+    .css('z-index', '1000')
+    .css('left', pos.left + ($(container).outerWidth() / 2) - 16)
+    .css('top', pos.top + ($(container).outerHeight() / 2) - 16)
+    .insertAfter(target);
+  
+  // Returns the element we added for cleanup.
+  return throbber;
 }
